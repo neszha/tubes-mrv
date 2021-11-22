b1c796739ce6c8d45ae63146123fa8fd9cd9bd5d
neszha <fanesahadi@gmail.com>
2021-11-22 15:52:19 +0700

    update komentar

--- program/module/OBEClass.py
+++ program/module/OBEClass.py
@@ -62,45 +62,12 @@ class OBE:
         size = 3
 
         for i in range(size):
+            # Membuat element di atas 1 bernilai 0.
             for j in range(size):
                 if i != j:
                     ratio = self.matrix[j][i] / self.matrix[i][i]
                     for k in range(size+1):
                         self.matrix[j][k] -= ratio * self.matrix[i][k]
-
-
-            # Membuat element di atas 1 bernilai 0.
-            # for j in range(0, i):
-            #     # Mencari titik di samping element 1 utama.
-            #     elm_point = 1
-            #     for j in range(i, col):
-            #         elm = self.matrix[i - 1][j]
-            #         if elm != 0:
-            #             elm_point = elm;
-            #             break
-            #
-            #     print(elm_point)
-
-                # Operasi eliminasi untuk membuat element 0.
-                # for j in range(col):
-                    # print(elm_point, self.matrix[i][j])
-                    # self.matrix[i - 1][j] -= elm_point * self.matrix[i][j]
-
-                # print(elm_point)
-
-                # elms_up = self.matrix[j]
-                # elms_point = self.matrix[i]
-
-                # Mencari element sebagai titik perhitungan.
-                # elm_poin = 1
-                # for a in range(len(elms_up)):
-                #     if elms_up[a] != 1 and elms_up[a] != 0:
-                #         elm_poin = elms_up[a]
-                #         break;
-
-                # Operasi eliminasi untuk membuat element 0.
-                # for k in range(col):
-                    # self.matrix[j][k] -= elm_poin * elms_point[k]
 
         return self.matrix
 
